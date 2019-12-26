74fc7c8d46d3cc3f26f4fd66565ef8935025d3a8
Jose Tandavala <jose.tandavala@gmail.com>
2019-12-26 19:05:14 +0000

    configuring bower

--- goodbuy.sh
+++ goodbuy.sh
@@ -27,6 +27,9 @@ create-react-app -v
 # Grunt - an automated task runner
 npm install -g grunt-cli
 
+# Bower - a dependency manager
+npm install -g bower
+
 # Installing mongoDB
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 2930ADAE8CAF5059EE73BB4B58712A2291FA4AD5
 sudo apt-get update
